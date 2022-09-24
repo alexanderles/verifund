26f50b712666634dee1a8e676f73b722c1c4204c
Alexander Les <awles642@gmail.com>
2022-09-24 19:26:21 -0400

    Added intial contracts

---
+++ contracts/interfaces/ICampaign.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+interface ICampaign {
+    function approve(address spender, uint value) external returns (bool);
+    function transfer(address to, uint value) external returns (bool);
+
+    function getTargetAmount() external view returns (uint256);
+    function getBalance() external view returns (uint256);
+    function getWhiteList() external view returns (address[] memory);
+    function getName() external view returns (string memory);
+
+    function withdraw(address, uint256) external returns (bool);
+} 
