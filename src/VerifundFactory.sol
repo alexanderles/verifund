70cefb36f592a89c1e6e5e99c8c74bbba721acc8
Ivan Rosales <ivan.gerard@crearth.com>
2022-09-25 00:13:19 -0400

    sheesh

--- src/VerifundFactory.sol
+++ src/VerifundFactory.sol
@@ -9,11 +9,15 @@ contract VerifundFactory is IVerifundFactory {
     Campaign[] private _campaigns;
 
     function createCampaign(string memory name, address[] memory whiteList, uint256 targetAmount) external {
-        Campaign campaign = new Campaign(name, whiteList, targetAmount);
+        Campaign campaign = new Campaign(whiteList, targetAmount);
         _campaigns.push(campaign);
     }
 
-    function allCampaigns(uint256 limit, uint256 offset) external view returns (Campaign[] memory coll) {
+    function allCampaigns(uint256 limit, uint256 offset) external pure returns (Campaign[] memory coll) {
         return coll;
+    }
+
+    function getAllCampaigns() external view returns (Campaign[] memory) {
+        return _campaigns;
     }
 }
